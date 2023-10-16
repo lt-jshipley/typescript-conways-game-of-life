eec96ad645f2ce6627a29d011182b22a5e7d9dde
lt-jshipley <jshipley@leantechniques.com>
2023-10-15 22:43:18 -0500

    common implementation example

--- src/game-of-life.spec.ts
+++ src/game-of-life.spec.ts
@@ -1,9 +1,174 @@
-// import { gameOfLife } from './game-of-life'
+import { newGame, nextIteration } from './game-of-life'
 
-describe('Game of Life', () => {
+describe('Game of Life: newGame', () => {
+  it('returns an empty board when starting a new game', () => {
+    // Act
+    const board = newGame(3);
 
-  it('should do all the things we want it to do', () => {
-    expect(true).toEqual(true);
+    // Assert
+    expect(board).toEqual([
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ])
   });
 
+  it('returns a board with starting live cells when passed in', () => {
+    // Act
+    const board = newGame(3, [
+      { x: 1, y: 1 }
+    ]);
+
+    // Assert
+    expect(board).toEqual([
+      [0, 0, 0],
+      [0, 1, 0],
+      [0, 0, 0],
+    ])
+  });
+});
+
+describe('Game of Life: nextIteration', () => {
+  it('dead game stays dead', () => {
+    // Arrange
+    const board = newGame(3);
+
+    // Act
+    const result = nextIteration(board);
+
+    // Assert
+    expect(result).toEqual([
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ])
+  });
+
+  it('cell dies when no living neighbors', () => {
+    // Arrange
+    const board = newGame(6, [
+      { x: 1, y: 1 },
+    ]);
+
+    // Act
+    const result = nextIteration(board);
+
+    // Assert
+    expect(result).toEqual([
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+    ]);
+  });
+
+  it('cell dies when only 1 living neighbors', () => {
+    // Arrange
+    const board = newGame(6, [
+      { x: 1, y: 1 },
+      { x: 1, y: 2 },
+    ]);
+
+    // Act
+    const result = nextIteration(board);
+
+    // Assert
+    expect(result).toEqual([
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+    ]);
+  });
+
+  it('cell stays alive when it has 2 living neighbors', () => {
+    // Arrange
+    const board = newGame(3, [
+      { x: 0, y: 0 },
+      { x: 0, y: 1 },
+      { x: 1, y: 0 },
+    ]);
+
+    // Act
+    const result = nextIteration(board);
+
+    // Assert
+    expect(result).toEqual([
+      [1, 1, 0],
+      [1, 1, 0],
+      [0, 0, 0],
+    ]);
+  });
+
+  it('cell stays alive when it has 3 living neighbors', () => {
+    // Arrange
+    const board = newGame(6, [
+      { x: 0, y: 0 },
+      { x: 1, y: 0 },
+      { x: 0, y: 1 },
+      { x: 1, y: 1 },
+    ]);
+
+    // Act
+    const result = nextIteration(board);
+
+    // Assert
+    expect(result).toEqual([
+      [1, 1, 0, 0, 0, 0],
+      [1, 1, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+    ]);
+  });
+
+  it('cell dies when it has 4 or more living neighbors', () => {
+    // Arrange
+    const board = newGame(3, [
+      { x: 0, y: 0 },
+      { x: 0, y: 1 },
+      { x: 0, y: 2 },
+      { x: 1, y: 1 },
+      { x: 2, y: 0 },
+      { x: 2, y: 1 },
+      { x: 2, y: 2 },
+    ]);
+
+    // Act
+    const result = nextIteration(board);
+
+    // Assert
+    expect(result).toEqual([
+      [1, 1, 1],
+      [0, 0, 0],
+      [1, 1, 1],
+    ]);
+  });
+
+  it('dead cell comes back to life when it has 3 living neighbors', () => {
+    // Arrange
+    const board = newGame(6, [
+      { x: 0, y: 0 },
+      { x: 0, y: 1 },
+      { x: 1, y: 0 },
+    ]);
+
+    // Act
+    const result = nextIteration(board);
+
+    // Assert
+    expect(result).toEqual([
+      [1, 1, 0, 0, 0, 0],
+      [1, 1, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+    ]);
+  });
 });
--- src/game-of-life.ts
+++ src/game-of-life.ts
@@ -1,5 +1,83 @@
-// NOTE: this has some starting simple game of life seeds.
-// import { MIXED_SEEDS } from '@src/util/gol-seeds';
+import { formatForConsoleOutput, cleanPrint, delay } from '@src/util/console.helpers';
+import { MIXED_SEEDS } from '@src/util/gol-seeds';
 
-export function gameOfLife() {
+type Location = {
+  x: number,
+  y: number
+}
+
+export async function gameOfLife() {
+  let board = newGame(25, MIXED_SEEDS);
+
+  let currentIteration = 0;
+  while(currentIteration < 50) {
+    board = nextIteration(board);
+    const output = formatForConsoleOutput(board);
+    cleanPrint(output);
+    await delay(500);
+    currentIteration++;
+  }
+}
+
+export function newGame(
+  gridSize: number, 
+  seed: Location[] | null = null
+): number[][] {
+  let board: number[][] = [];
+  for(let rowIndex = 0; rowIndex < gridSize; rowIndex++) {
+    let row = [];
+    for(let columnIndex = 0; columnIndex < gridSize; columnIndex++) {
+      row.push(0);
+    }
+    board.push(row);
+  }
+
+  if(seed) {
+    seed.forEach((s) => {
+      board[s.x][s.y] = 1
+    });
+  }
+
+  return board;
+}
+
+export function nextIteration(board: number[][]) {
+  let nextBoard: number[][] = [];
+  for(let rowIndex = 0; rowIndex < board.length; rowIndex++) {
+    let row = [];
+    for(let columnIndex = 0; columnIndex < board.length; columnIndex++) {
+      const priorCellState = board[rowIndex][columnIndex];
+      const liveNeighbors = getLiveNeighborsCount(board, { x: rowIndex, y: columnIndex });
+      let cellState = priorCellState;
+      if(priorCellState === 1) {
+        if(liveNeighbors < 2 || liveNeighbors > 3) {
+          cellState = 0;
+        }
+      } else {
+        if(liveNeighbors === 3) {
+          cellState = 1;
+        }
+      }
+      row.push(cellState);
+    }
+    nextBoard.push(row);
+  }
+
+  return nextBoard;
+}
+
+function getLiveNeighborsCount(board: number[][], location: Location): number {
+  const neighbors = [
+    { x: location.x - 1, y: location.y - 1 }, { x: location.x - 1, y: location.y + 0 }, { x: location.x - 1, y: location.y + 1 }, 
+    { x: location.x + 0, y: location.y - 1 },                                           { x: location.x + 0, y: location.y + 1 }, 
+    { x: location.x + 1, y: location.y - 1 }, { x: location.x + 1, y: location.y + 0 }, { x: location.x + 1, y: location.y + 1 }, 
+  ];
+
+  let liveCount = 0;
+  neighbors.forEach((n) => {
+    if(n.x >= 0 && n.y >= 0 && n.x < board.length && n.y < board.length) {
+      liveCount += board[n.x][n.y];
+    }
+  });
+  return liveCount;
 }
